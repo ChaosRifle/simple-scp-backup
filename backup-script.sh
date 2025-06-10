b1f1ed5b3df19e8d1620d13fac650db494b96724
ChaosRifle <DC3120@gmail.com>
2025-06-10 16:13:44 -0600

    Update backup-script.sh

--- backup-script.sh
+++ backup-script.sh
@@ -2,16 +2,17 @@
 #latest, archive (datestamped folders), and teamspeak structure(entire TS backup)
 USERNAME="replaceme"
 IP="replaceme"
+SSHPRIVATEKEY="~/.ssh/backup-teamspeak"
 
 DATESTAMP="$(date +%y\\%m\\%d-%H:%M:%S)"
 mkdir ~/teamspeak-backup/archive/$DATESTAMP
 
-scp -i ~/.ssh/backup-teamspeak $USERNAME@$IP:/home/teamspeak/teamspeak/licensekey.dat ~/teamspeak-backup/archive/$DATESTAMP
-scp -i ~/.ssh/backup-teamspeak $USERNAME@$IP:/home/teamspeak/teamspeak/query_ip_blacklist.txt ~/teamspeak-backup/archive/$DATESTAMP
-scp -i ~/.ssh/backup-teamspeak $USERNAME@$IP:/home/teamspeak/teamspeak/query_ip_whitelist.txt ~/teamspeak-backup/archive/$DATESTAMP
-scp -i ~/.ssh/backup-teamspeak $USERNAME@$IP:/home/teamspeak/teamspeak/ts3server.sqlitedb ~/teamspeak-backup/archive/$DATESTAMP
-scp -i ~/.ssh/backup-teamspeak $USERNAME@$IP:/home/teamspeak/teamspeak/ts3server.sqlitedb-shm ~/teamspeak-backup/archive/$DATESTAMP
-scp -i ~/.ssh/backup-teamspeak $USERNAME@$IP:/home/teamspeak/teamspeak/ts3server.sqlitedb-wal ~/teamspeak-backup/archive/$DATESTAMP
+scp -i $SSHPRIVATEKEY $USERNAME@$IP:/home/teamspeak/teamspeak/licensekey.dat ~/teamspeak-backup/archive/$DATESTAMP
+scp -i $SSHPRIVATEKEY $USERNAME@$IP:/home/teamspeak/teamspeak/query_ip_blacklist.txt ~/teamspeak-backup/archive/$DATESTAMP
+scp -i $SSHPRIVATEKEY $USERNAME@$IP:/home/teamspeak/teamspeak/query_ip_whitelist.txt ~/teamspeak-backup/archive/$DATESTAMP
+scp -i $SSHPRIVATEKEY $USERNAME@$IP:/home/teamspeak/teamspeak/ts3server.sqlitedb ~/teamspeak-backup/archive/$DATESTAMP
+scp -i $SSHPRIVATEKEY $USERNAME@$IP:/home/teamspeak/teamspeak/ts3server.sqlitedb-shm ~/teamspeak-backup/archive/$DATESTAMP
+scp -i $SSHPRIVATEKEY $USERNAME@$IP:/home/teamspeak/teamspeak/ts3server.sqlitedb-wal ~/teamspeak-backup/archive/$DATESTAMP
 
 #rm -rf ~/teamspeak-backup/latest/*
 #cp ~/teamspeak-backup/archive/$DATESTAMP ~/teamspeak-backup/latest/
